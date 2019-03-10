b932d94f99e8f170e6ff320d2e7b02a550e8f114
Chengyuan <icebergmatt0109@gmail.com>
2019-03-10 20:27:35 +0100

    naive implementation of svd (code not cleaned)

---
+++ parameters.py
@@ -0,0 +1,10 @@
+# defining paths and paramters
+
+RAWDATA_PATH = './data/data_train.csv'
+SAMPLECSV_PATH = './data/sampleSubmission.csv'
+OUTPUTCSV_PATH = './out.csv'
+MATRAW_PATH = './cache/matraw.npy'
+MATMEAN_PATH = './cache/matimpute.npy'
+
+NCOLS = 1000
+NROWS = 10000
