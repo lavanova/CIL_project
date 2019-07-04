4d37204767741c48a44d9091c1a4a6cf1b608e54
alphabetsean <seanhuangxinyuan@gmail.com>
2019-07-04 19:46:14 +0200

    init encoder

--- encoder/init_encoder.sh
+++ encoder/init_encoder.sh
@@ -1,16 +1,3 @@
-
-if [ -d ../cache ]; then
-	echo "Cache directory already exist"
-else
-	mkdir cache
-fi
-
-if [ -d ../test ]; then
-	echo "Test directory already exist"
-else
-	mkdir test
-fi
-
 if [ -d ./data ]; then
 echo "Data directory already exist"
 else
